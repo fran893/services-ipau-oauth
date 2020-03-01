5489453dfcf8412724634df7ea0026ee0ab739d9
Franco <alvarez.fr893@gmail.com>
2020-03-01 10:13:26 -0300

    primer commit

---
+++ services-ipau-oauth/src/main/java/com/app/ipau/oauth/ServicesIpauOauthApplication.java
@@ -0,0 +1,13 @@
+package com.app.ipau.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesIpauOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesIpauOauthApplication.class, args);
+	}
+
+}
---
+++ services-ipau-oauth/src/test/java/com/app/ipau/oauth/ServicesIpauOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ipau.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesIpauOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
